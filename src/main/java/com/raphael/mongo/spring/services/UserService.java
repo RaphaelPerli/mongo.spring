d06539793309c133384f2251eb1807434880cbd4
RaphaelPerli <rapha.perle@hotmail.com>
2026-04-16 15:41:56 -0300

    Obtendo um usuario por id

--- src/main/java/com/raphael/mongo/spring/services/UserService.java
+++ src/main/java/com/raphael/mongo/spring/services/UserService.java
@@ -1,12 +1,14 @@
 package com.raphael.mongo.spring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.raphael.mongo.spring.domain.User;
 import com.raphael.mongo.spring.repository.UserRepository;
+import com.raphael.mongo.spring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    Optional<User> user = repo.findById(id);
+	    if (!user.isPresent()) {
+	        throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+	    return user.get();
 	}
 }
